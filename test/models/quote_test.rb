09afa00fdab20c424754a2ba0100d7a71a8a3c8d
PYLiao <lby75429@gmail.com>
2015-11-17 03:04:51 +0000

    test success and add unique_tag method in root page

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -5,8 +5,12 @@ class QuoteTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "the turth" do
-  	assert true
+  test "unique_tag" do
+  	quote = Quote.create(:author => "Bonga Saluda", :saying => "You are so handsome")
+  	expected = "BS#" + quote.id.to_s
+  	actual = quote.unique_tag
+  	#trigger "unique_tag" method on the test record of the database.
+  	assert_equal expected, actual 
+  	#A method called "assert_equal" cause the test to fail if two values are not equal
   end
-
 end
