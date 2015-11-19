d1ac76d6eea43bf4bfaf45289b47d6fe4e73128c
PYLiao <lby75429@gmail.com>
2015-11-19 20:03:32 +0000

    building controller test

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -4,4 +4,15 @@ class QuotesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "quote show page" do
+  	quote = Quote.create(:author => 'Bonga Liao', :saying => 'Say hallo to you.')
+  	get :show, :id => quote.id
+  	assert_response :success
+  end
+
+  test "quote show page, not found" do
+  	get :show, :id => 'OMG'
+  	assert_response :not_found
+  end
 end
